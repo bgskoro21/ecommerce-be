1e9b6cc5691aa06620da60c14448f96f20232806
bgskoro21 <bagaskara_dwi_putra@teknokrat.ac.id>
2024-10-02 18:42:14 +0700

    fix: redirect uri to oauth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -130,7 +130,7 @@ export class UserService {
         code,
         client_id: process.env.GOOGLE_CLIENT_ID,
         client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        redirect_uri: 'http://localhost:3000/api/users/google-login',
+        redirect_uri: 'http://localhost:3000/api/auth/callback',
         grant_type: 'authorization_code',
       }),
     );
